b0b2780c46e6c73a80bb37217d15bcd7f56b8ad0
DenisSakic <denis.sakic04@gmail.com>
2023-12-28 21:36:51 +0100

    Search

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,61 @@ window.addEventListener("scroll", function () {
   }
 
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const searchBtn = document.querySelector('.search-btn');
+  const headerBtnGroup = document.querySelector('.header-btn-group');
+  const searchContainer = document.createElement('div');
+  searchContainer.className = 'search-container';
+  searchContainer.id = 'searchContainer';
+
+  // Dodajte input polje i gumb za zatvaranje u pretraživač
+  searchContainer.innerHTML = `
+    <input type="text" id="searchInput" placeholder="Type your search...">
+    <button class="close-btn" id="closeBtn" aria-label="Close">
+      <ion-icon name="close"></ion-icon>
+    </button>
+  `;
+
+  // Dodajte pretraživač unutar header-btn-group
+  headerBtnGroup.appendChild(searchContainer);
+
+  const searchInput = document.getElementById('searchInput');
+  const closeBtn = document.getElementById('closeBtn');
+
+  // Onemogućite automatski prikaz pretraživača prilikom učitavanja stranice
+  searchContainer.style.display = 'none';
+
+  // Funkcija za prikazivanje ili sakrivanje pretraživača
+  function toggleSearchContainer() {
+    if (searchContainer.style.display === 'none') {
+      searchContainer.style.display = 'block';
+      searchInput.focus();
+    } else {
+      searchContainer.style.display = 'none';
+    }
+  }
+
+  // Prikazivanje ili sakrivanje pretraživača kada se klikne na gumb za pretraživanje
+  searchBtn.addEventListener('click', function (e) {
+    e.stopPropagation(); // Spriječava daljnje širenje klika prema gore
+    toggleSearchContainer();
+  });
+
+  // Sakrij pretraživač kada se klikne na gumb za zatvaranje
+  closeBtn.addEventListener('click', function () {
+    searchContainer.style.display = 'none';
+  });
+
+  // Sakrij pretraživač kada se klikne izvan njega
+  document.addEventListener('click', function (event) {
+    if (!searchContainer.contains(event.target) && event.target !== searchBtn) {
+      searchContainer.style.display = 'none';
+    }
+  });
+
+  // Dodajte stilizaciju bijelih slova u inputu
+  searchInput.style.color = 'white';
+  closeBtn.style.display = 'none'
+});
+
